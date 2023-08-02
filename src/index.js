45f34246c6db05ed7f73657b649ee8e619aceb55
WillJolley <yeswilljolley@gmail.com>
2023-08-01 19:12:16 -0700

    add some UI

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Subject from './ageCalc.js';
 
+function handleForm(event) {
+  event.preventDefault();
+  const userAge = parseInt(document.getElementById("userAge"));
+  const pastAge = parseInt(document.getElementById("pastAge"));
+  const futureAge = parseInt(document.getElementById("futureAge"));
+  
+  let subject = new Subject(userAge, pastAge, futureAge);
+  subject.calculateAges();
+  subject.calcEarthYearsPassed();
+  subject.calcYearsPassed();
+  subject.calcEarthYearsToPass();
+  subject.calcYearsToPass();
+  
+  let results = document.getElementById("results")
+  results.ul.append 
+  
+}
+
+window.addEventListener("load", function() {
+  this.document.querySelector('form').addEventListener("submit", handleForm);
+});
